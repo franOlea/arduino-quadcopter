d57730f48c7d172fdaf09f8f8c8d9d064a3478f2
francisco.olea <francisco.olea@jpmorgan.com>
2017-03-20 17:57:17 -0300

    Makes some progress on main program

--- flight_controller/flight_controller.ino
+++ flight_controller/flight_controller.ino
@@ -1,15 +1,30 @@
+#include "EEPROM.h"
 #include "Barometer.h"
 #include "PidController.h"
 #include "MPU6050.h"
 
+byte EEPROM_DATA[36];
+
 MPU6050 gyroscope;
 Barometer barometer;
+
+
 double altitude = 0;
 
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println("REBOOT");
-  barometer.initialize();
+	readEEPROM();
+	setOutputPins();
+	
+	if(isSystemReady() != 0) {
+		//TODO Led error infinite loop
+	}
+
+//	MPU6050.initialize();
+
+//  Serial.begin(9600);
+//  Serial.println("REBOOT");
+//  barometer.initialize();
 }
 
 void loop() {
@@ -20,4 +35,26 @@ void loop() {
   }
   Serial.println("alt: " + String(altitude/12));
   altitude = 0;
+}
+
+void readEEPROM() {
+	for(int i=0; i < 36; i++) {
+		EEPROM_DATA[i] = EEPROM.read(i);
+	}
+}
+
+void setOutputPins() {
+	//TODO maps and sets output pins (ESCs & LED)
+}
+
+int isSystemReady() {
+	if(EEPROM_DATA[33] != 'J' || EEPROM_DATA[34] != 'M' || EEPROM_DATA['B']) {
+		return -1;
+	}
+
+	if(EEPROM_DATA[31] == 2 || EEPROM_DATA[31] == 3) {
+		return -2;
+	}
+
+	return 0;
 }
